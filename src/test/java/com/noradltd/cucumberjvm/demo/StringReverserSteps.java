181e6c225f6aa012451bfc952798ae54bbf75f53
Rich Dammkoehler <rpd@noradltd.com>
2014-06-10 12:53:46 -0500

    finished steps, now to TDD the actual implementation

--- src/test/java/com/noradltd/cucumberjvm/demo/StringReverserSteps.java
+++ src/test/java/com/noradltd/cucumberjvm/demo/StringReverserSteps.java
@@ -1,15 +1,17 @@
 package com.noradltd.cucumberjvm.demo;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.runtime.PendingException;
 
 public class StringReverserSteps {
 
 	private StringReverser reverser;
 	private String actual;
-	
+
 	@Given("^a String Reverser$")
 	public void a_String_Reverser() throws Throwable {
 		reverser = new StringReverser();
@@ -17,12 +19,12 @@ public class StringReverserSteps {
 
 	@When("^I reverse the string \"([^\"]*)\"$")
 	public void I_reverse_the_string(String input) throws Throwable {
-	    actual = reverser.reverse(input);
+		actual = reverser.reverse(input);
 	}
 
 	@Then("^the result is \"([^\"]*)\"$")
 	public void the_result_is(String expected) throws Throwable {
-	    throw new PendingException();
+		assertThat(actual, is(expected));
 	}
-	
+
 }
