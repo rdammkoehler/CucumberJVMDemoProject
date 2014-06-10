c050278de4a8bd80d6d09fb0e4027733df44b280
Rich Dammkoehler <rpd@noradltd.com>
2014-06-10 12:43:34 -0500

    added step template for StringReverser

---
+++ src/test/java/com/noradltd/cucumberjvm/demo/StringReverserSteps.java
@@ -0,0 +1,25 @@
+package com.noradltd.cucumberjvm.demo;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.runtime.PendingException;
+
+public class StringReverserSteps {
+
+	@Given("^a String Reverser$")
+	public void a_String_Reverser() throws Throwable {
+	    throw new PendingException();
+	}
+
+	@When("^I reverse the string \"([^\"]*)\"$")
+	public void I_reverse_the_string(String arg1) throws Throwable {
+	    throw new PendingException();
+	}
+
+	@Then("^the result is \"([^\"]*)\"$")
+	public void the_result_is(String arg1) throws Throwable {
+	    throw new PendingException();
+	}
+	
+}
