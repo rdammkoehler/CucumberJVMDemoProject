ff5fcb9afdd74f68078f5faa5bf7345a67fb23f2
Rich Dammkoehler <rpd@noradltd.com>
2014-06-10 12:58:08 -0500

    first basic behavior

--- src/main/java/com/noradltd/cucumberjvm/demo/StringReverser.java
+++ src/main/java/com/noradltd/cucumberjvm/demo/StringReverser.java
@@ -3,7 +3,7 @@ package com.noradltd.cucumberjvm.demo;
 public class StringReverser {
 
 	public String reverse(String input) {
-		return null;
+		return "";
 	}
 
 }
---
+++ src/test/java/com/noradltd/cucumberjvm/demo/ReverseBasics.java
@@ -0,0 +1,16 @@
+package com.noradltd.cucumberjvm.demo;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ReverseBasics {
+
+	private static final String EMPTY_STRING = "";
+
+	@Test
+	public void emptyStringIsReversedToEmptyString() {
+		assertThat(new StringReverser().reverse(EMPTY_STRING), is(EMPTY_STRING));
+	}
+}
