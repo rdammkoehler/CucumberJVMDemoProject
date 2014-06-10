1fae57ebe1d47c5e99453255f5ff4adf1e3b0bbc
Rich Dammkoehler <rpd@noradltd.com>
2014-06-10 13:04:29 -0500

    begin refactor

--- src/test/java/com/noradltd/cucumberjvm/demo/ReverseBasics.java
+++ src/test/java/com/noradltd/cucumberjvm/demo/ReverseBasics.java
@@ -8,10 +8,14 @@ import org.junit.Test;
 public class ReverseBasics {
 
 	private static final String EMPTY_STRING = "";
+	
+	private String reverse(String input) {
+		return new StringReverser().reverse(input);
+	}
 
 	@Test
 	public void emptyStringIsReversedToEmptyString() {
-		assertThat(new StringReverser().reverse(EMPTY_STRING), is(EMPTY_STRING));
+		assertThat(reverse(EMPTY_STRING), is(EMPTY_STRING));
 	}
 
 	@Test
